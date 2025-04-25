366892e10184519edff88b277b370883c9c3a042
金子唯斗 <k024g1019@g.neec.ac.jp>
2025-04-25 15:35:01 +0900

    多分完成

--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
 #include"stdio.h"
 
+//①template<typename Type>と書いてテンプレートを定義
+template<typename Type1, typename Type2>
+//②引数を①で宣言・定義したものとする
+Type1 add(Type1 a, Type2 b) {
+	//③戻り値は員数によって変化するので、キャスと(明示的な型変化)を使って整える
+	if (a<b)
+	{
+		return a;
+	}
+	if (b<a)
+	{
+		return b;
+	}
+}
+template<>
+char add<char>(char a, char b) {
+	return printf("数字以外を入れるな　カス\n");
+}
+
 int main() {
-
-	printf("今日は暑いね！！");
-
-	return 0;
+	printf("%d\n", add<int, float>(114, 51.4f));
+	printf("%d\n", add<char, char>(110,100));
+	printf("%d\n", add<double, double>(100, 100));
 }
